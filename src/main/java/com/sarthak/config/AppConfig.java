089312ef0fe9fe932d749c488a73e1e59d3b1364
Sarthak Rastogi <rsarthakverse123@gmail.com>
2024-09-05 20:07:32 +0530

    altered cors policy

--- src/main/java/com/sarthak/config/AppConfig.java
+++ src/main/java/com/sarthak/config/AppConfig.java
@@ -90,7 +90,7 @@ public class AppConfig {
                         "http://localhost:5173",
                         "http://localhost:5174",
                         "http://localhost:4200",
-                        "https://tradexpert-backend.onrender.com/"
+                        "https://tradexpert-backend.onrender.com"
                 ));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
